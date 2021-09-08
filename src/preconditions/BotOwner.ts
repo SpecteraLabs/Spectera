b49a5d5506ca36732b005bf220443057d661bb6d
UndiedGamer <hemanthchakka2007@gmail.com>
2021-09-08 12:19:56 +0530

    better: owneronly precondition

--- src/preconditions/BotOwner.ts
+++ src/preconditions/BotOwner.ts
@@ -1,8 +1,9 @@
+import { OWNERS } from '#root/config';
 import { Precondition, PreconditionResult } from '@sapphire/framework';
 import type { Message } from 'discord.js';
 
 export class BotOwner extends Precondition {
 	public run(message: Message): PreconditionResult {
-		return message.author.id === '564468550727761920' ? this.ok() : this.error({ message: 'This command can only be used by bot owner. ' });
+		return OWNERS.includes(message.author.id) ? this.ok() : this.error({ message: 'This command can only be used by bot owner. ' });
 	}
 }
