9d3e4383ef3578f01d496c622e897ef20ff7929e
UndiedGamer <hemanthchakka2007@gmail.com>
2021-10-04 09:13:21 +0530

    feat(colorette): fix deprecations

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -4,9 +4,9 @@ import 'reflect-metadata';
 import '@sapphire/plugin-logger/register';
 import '@sapphire/plugin-api/register';
 import '@sapphire/plugin-editable-commands/register';
-import { options as coloretteOptions } from 'colorette';
+import * as colorette from 'colorette';
 import { inspect } from 'util';
 
 inspect.defaultOptions.depth = 1;
 
-coloretteOptions.enabled = true;
+colorette.createColors({ useColor: true });
