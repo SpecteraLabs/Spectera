feaf82777ca5141e190a470f9241a96ad80dced0
UndiedGamer <hemanthchakka2007@gmail.com>
2022-01-23 11:50:57 +0530

    chore: move to @sapphire/phisherman

--- src/listeners/message/AntiPhish.ts
+++ src/listeners/message/AntiPhish.ts
@@ -1,9 +1,8 @@
-import { buildPhishLog, phisherFetch, uriRegex } from '#lib/constants';
-import { isScam } from '#lib/utils/util';
+import { buildPhishLog, uriRegex } from '#lib/constants';
 import { PHISHERMAN_KEY } from '#root/config';
 import { ApplyOptions } from '@sapphire/decorators';
-import { fetch } from '@sapphire/fetch';
 import { Listener, ListenerOptions } from '@sapphire/framework';
+import { checkDomain } from '@sapphire/phisherman';
 import type { Message, TextChannel } from 'discord.js';
 
 @ApplyOptions<ListenerOptions>({
@@ -14,32 +13,15 @@ export class AntiPhishEvent extends Listener {
 	public async run(message: Message) {
 		if (message.author.bot) return;
 		if (!message.guild) return;
+		console.log(message.content);
 		if (!message.content.match(uriRegex)) return;
 		try {
 			const execedUri: string | RegExpExecArray = uriRegex.exec(message.content)!;
 			const uri = execedUri[0].replace('www.', '');
-			const check = await fetch<phisherFetch>(`https://api.phisherman.gg/v2/domains/check/${uri}`, {
-				headers: {
-					'Content-Type': 'application/json',
-					Authorization: `Bearer ${PHISHERMAN_KEY}`
-				}
-			});
-			if (isScam(check.classification)) {
+			const check = await checkDomain(uri, PHISHERMAN_KEY);
+
+			if (check.isScam) {
 				await message.delete();
-				// await fetch(
-				// 	`https://api.phisherman.gg/v1/domains/${uri}`,
-				// 	{
-				// 		method: FetchMethods.Put,
-				// 		headers: {
-				// 			'Content-Type': 'application/json',
-				// 			Authorization: `Bearer ${PHISHERMAN_KEY}`
-				// 		},
-				// 		body: JSON.stringify({
-				// 			guild: message.guild.id
-				// 		})
-				// 	},
-				// 	FetchResultTypes.Result
-				// );
 				const result = await this.container.database.guildLogs.findUnique({
 					where: {
 						id: message.guild.id
